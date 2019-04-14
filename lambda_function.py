a3724813b2f87de296831eab8a8ec92c369f1dcc
Tsubasa Ogawa <hurikake@gmail.com>
2019-04-14 05:02:44 +0000

    make it possible to obtain parcels and dates from yamato email

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,26 @@
-import httplib2, os
+import base64
+import chardet
+import email
+import os
+import re
+import sys
+import httplib2
 from apiclient import discovery
-import gmail_auth # 先ほど作成したプログラム
+import gmail_auth
 
-# Gmailのサービスを取得
-def gmail_get_service():
+
+SEARCH_QUERY = 'from: mail@kuronekoyamato.co.jp AND (subject: お荷物お届けのお知らせ OR subject: 再配達依頼受付完了のお知らせ)'
+PATTERNS = {
+    'parcel_and_date': [
+      r"(.+) 様からのお荷物を　【(.+)】　にお届け予定です。",
+    ],
+    'date_only': [
+      r"(.+)にお届けに参りましたが、ご不在でしたので持ち帰りました。",
+      r"■ご希望日時　　　：(.+)",
+    ]
+}
+
+def get_gmail_service():
     # ユーザー認証の取得
     credentials = gmail_auth.gmail_user_auth()
     http = credentials.authorize(httplib2.Http())
@@ -11,21 +28,55 @@ def gmail_get_service():
     service = discovery.build('gmail', 'v1', http=http)
     return service
 
-# メッセージの一覧を取得
-def gmail_get_messages():
-    service = gmail_get_service()
+
+def get_messages():
+    service = get_gmail_service()
+
     # メッセージの一覧を取得
     messages = service.users().messages()
-    msg_list = messages.list(userId='me', maxResults=10).execute()
+    msg_list = messages.list(
+        userId='me',
+        maxResults=3,
+        q=_generate_query_from_yamato()
+    ).execute()
 
-    # 取得したメッセージの一覧を表示
+    msgs = []
     for msg in msg_list['messages']:
-        topid = msg['id']
-        msg = messages.get(userId='me', id=topid).execute()
-        print("---")
-        print(msg['snippet']) # 要約を表示
+        msg = messages.get(userId='me', id=msg['id'], format='raw').execute()
+
+        raw_msg = base64.urlsafe_b64decode(msg['raw'])
+        email_msg = email.message_from_bytes(raw_msg)
+        msgs.append(email_msg.get_payload(decode=True).decode('iso-2022-jp'))
+
+    return msgs
+
+def _generate_query_from_yamato():
+    return SEARCH_QUERY
 
 
 if __name__ == '__main__':
+  patterns = {}
+  for key, part_of_patterns in PATTERNS.items():
+    patterns[key] = list(map(lambda v: re.compile(v), part_of_patterns))
+
   # メッセージの取得を実行
-  gmail_get_messages()
+  messages = get_messages()
+  for message in messages:
+    result = {}
+    for pattern in patterns['parcel_and_date']:
+      match = pattern.search(message)
+      if match:
+        result['parcel'] = match.group(1)
+        result['date'] = match.group(2)
+        print('{0}: {1}'.format(match.group(1), match.group(2)))
+        break
+    if not result:
+        for pattern in patterns['date_only']:
+          match = pattern.search(message)
+          if match:
+            result['date'] = match.group(1)
+            print('{0}'.format(match.group(1)))
+            break
+    if not result:
+      print('No Info.')
+
